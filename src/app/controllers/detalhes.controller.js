bd3737788b1c470edc1388483bc8bff4d59e930f
rafaelbitencourt <rafael_silbit@hotmail.com>
2021-06-20 18:39:37 -0300

    Editar detalhe

--- src/app/controllers/detalhes.controller.js
+++ src/app/controllers/detalhes.controller.js
@@ -29,6 +29,35 @@ class DetalhesController extends BaseController {
         }
     }
 
+    async update(req, res) {
+        try {
+            if (!req.body) {
+                res.status(400).json({
+                    message: "O conteúdo não pode ser vazio."
+                });
+            }
+
+            var dados = req.body;
+        
+            if (!req.file) {
+                res.status(400).json({
+                    message: "É necessário informar a imagem do detalhe."
+                });
+                return;
+            }
+        
+            dados.imagem = req.file.buffer;
+
+            const result = await this.getModel(req.tenantId).findByPk(req.params.id);
+
+            await result.update(dados);
+
+            return res.json({ result });
+        } catch (err) {
+            return res.status(500).json(err);
+        }
+    }
+
     async findPorProjeto(req, res) {
         try {
             const detalhes = await this.getModel(req.tenantId).findAll({
